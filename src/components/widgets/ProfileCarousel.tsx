0f544a3c2e4fbbecda74fd75d552f3daec9f1c57
Enzo Lima <129903213+thenzolima@users.noreply.github.com>
2025-07-25 17:56:49 -0300

    feat: melhoramento do aboutPages

--- src/components/widgets/ProfileCarousel.tsx
+++ src/components/widgets/ProfileCarousel.tsx
@@ -4,7 +4,11 @@ import img01 from "@/assets/profiles/01.webp";
 import img02 from "@/assets/profiles/02.webp";
 import img03 from "@/assets/profiles/03.webp";
 
-export function ProfileCarousel() {
+interface ProfileCarouselProps {
+  style: string;
+}
+
+export function ProfileCarousel({ style }: ProfileCarouselProps) {
   const images = [img01, img02, img03];
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -16,11 +20,11 @@ export function ProfileCarousel() {
   }, [images.length]);
 
   return (
-    <div className="w-[100px] h-[100px] rounded-sm overflow-hidden border-2">
+    <div className={style}>
       <img
         src={images[currentIndex].src}
         alt={`Foto de perfil 0${currentIndex + 1}`}
-        className="object-cover w-[100px] h-[100px]"
+        className="object-cover"
       />
     </div>
   );
