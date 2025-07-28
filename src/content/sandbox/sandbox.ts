f4a3fe5b0f9cce5bdd8c84abe77204de8ca1f8e0
Enzo Lima <129903213+thenzolima@users.noreply.github.com>
2025-07-28 18:25:59 -0300

    feat: adiciona o widget sandbox

---
+++ src/content/sandbox/sandbox.ts
@@ -0,0 +1,25 @@
+const sandboxData = [
+  {
+    name: "Scot Scot",
+    description: "Desenvolvedora",
+    icon: "/assets/sandbox/SCOT.webp",
+    category: "Branding",
+    date: "OUT 2022",
+  },
+  {
+    name: "Meu Cartão TEA",
+    description: "SaaS ",
+    icon: "/assets/sandbox/TEA.webp",
+    category: "Branding",
+    date: "JUL 2025",
+  },
+  {
+    name: "Labrau",
+    description: "Coletivo Criativo",
+    icon: "/assets/sandbox/LABRAU.webp",
+    category: "Branding",
+    date: "JUL 2025",
+  },
+];
+
+export default sandboxData;
